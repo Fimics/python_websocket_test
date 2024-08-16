8d8f061d6dfe67f33f99b53a77a06d559194318b
mac <lpjhblpj@163.com>
2024-08-16 15:02:42 +0800

    python 作用客户端 测试功能通过

---
+++ python_multimodal_single/client2_test.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+
+
+async def send_message():
+    uri = "ws://192.168.2.93:49999"  # WebSocket 服务器的地址
+
+    while True:
+        try:
+            async with websockets.connect(uri, ping_interval=5, ping_timeout=200) as websocket:
+                print("Connected to the server.")
+                while True:
+                    message = input("Enter a message to send to the server: ")
+                    await websocket.send(message)
+                    response = await websocket.recv()
+                    print(f"Received response from server: {response}")
+        except websockets.exceptions.ConnectionClosedError as e:
+            print(f"Connection closed unexpectedly: {e}")
+        except websockets.exceptions.WebSocketException as e:
+            print(f"WebSocket exception occurred: {e}")
+
+        print("Reconnecting...")
+        await asyncio.sleep(5)  # 重新连接前等待5秒
+
+
+asyncio.get_event_loop().run_until_complete(send_message())
